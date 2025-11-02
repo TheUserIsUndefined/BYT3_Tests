91437981f01b411e39cc873d40ec174751f4a762
TheUserIsUndefined <s31354@pjwstk.edu.pl>
2025-11-02 19:28:41 +0100

    Implement Tests for Figures

---
+++ Figures/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Figures;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        IShape sphere = new Sphere(5);
+        Console.WriteLine("Sphere:");
+        Console.WriteLine($"Area: {sphere.CalculateArea():F3}");
+        Console.WriteLine($"Volume: {sphere.CalculateVolume():F3}");
+        Console.WriteLine();
+
+        IShape cylinder = new Cylinder(3, 7);
+        Console.WriteLine("Cylinder:");
+        Console.WriteLine($"Area: {cylinder.CalculateArea():F3}");
+        Console.WriteLine($"Volume: {cylinder.CalculateVolume():F3}");
+        Console.WriteLine();
+
+        IShape rectangle = new Rectangle(4, 8);
+        Console.WriteLine("Rectangle:");
+        Console.WriteLine($"Area: {rectangle.CalculateArea():F3}");
+        
+        Console.WriteLine($"Volume: {rectangle.CalculateVolume():F3}");
+        Console.WriteLine();
+
+        IShape cube = new Cube(4);
+        Console.WriteLine("Cube:");
+        Console.WriteLine($"Area: {cube.CalculateArea():F3}");
+        Console.WriteLine($"Volume: {cube.CalculateVolume():F3}");
+    }
+}
---
+++ Tests/CalculatorTests.cs
@@ -0,0 +1,65 @@
+﻿using BYT3_Calculator;
+using BYT3_Calculator.Exceptions;
+
+namespace Tests
+{
+    public class CalculatorTests
+    {
+        private readonly Calculator _add = new Calculator(12345, 54321, '+');
+        private readonly Calculator _sub = new Calculator(23456, 65432, '-');
+        private readonly Calculator _mul = new Calculator(34567, 76543, '*');
+        private readonly Calculator _div = new Calculator(45678, 91356, '/');
+        private readonly Calculator _divByZero = new Calculator(1, 0, '/');
+        private readonly Calculator _unknownOp = new Calculator(1, 1, '^');
+        private readonly Calculator _boundariesOverflow = new Calculator(1e155, 1e155, '*');
+        private readonly Calculator _boundariesUnderflow = new Calculator(-1e155, 1e155, '*');
+        
+        [Test]
+        public void TestAddition()
+        {
+            Assert.That(_add.Calculate(), Is.EqualTo(66666));
+        }
+
+        [Test]
+        public void TestSubtraction()
+        {
+            Assert.That(_sub.Calculate(), Is.EqualTo(-41976));
+        }
+
+        [Test]
+        public void TestMultiplication()
+        {
+            Assert.That(_mul.Calculate(), Is.EqualTo(2645861881));
+        }
+
+        [Test]
+        public void TestDivision()
+        {
+            Assert.That(_div.Calculate(), Is.EqualTo(0.5));
+        }
+
+        [Test]
+        public void TestDivisionByZero()
+        {
+            Assert.Throws<DivideByZeroException>(() => _divByZero.Calculate());
+        }
+
+        [Test]
+        public void TestUnknownOperation()
+        {
+            Assert.Throws<UnknownOperationException>(() => _unknownOp.Calculate());
+        }
+
+        [Test]
+        public void TestBoundariesOverflow()
+        {
+            Assert.Throws<BoundariesExceededException>(() => _boundariesOverflow.Calculate());
+        }
+        
+        [Test]
+        public void TestBoundariesUnderflow()
+        {
+            Assert.Throws<BoundariesExceededException>(() => _boundariesUnderflow.Calculate());
+        }
+    }
+}
